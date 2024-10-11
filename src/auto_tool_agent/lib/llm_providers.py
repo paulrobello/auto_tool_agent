d86af813043edabe8752ec747ed75110cf88a548
Paul Robello <probello@gmail.com>
2024-10-11 15:26:59 -0700

    update llm core

--- src/auto_tool_agent/lib/llm_providers.py
+++ src/auto_tool_agent/lib/llm_providers.py
@@ -1,8 +1,21 @@
 """LLM provider types."""
 
 from __future__ import annotations
+
 import os
+from dataclasses import dataclass
 from enum import Enum
+from typing import Optional
+
+
+@dataclass
+class LangChainConfig:
+    """Langchain config."""
+
+    tracing: bool = False
+    project: str = "parllama"
+    base_url: str = "https://api.smith.langchain.com"
+    api_key: str = ""
 
 
 class LlmProvider(str, Enum):
@@ -16,12 +29,24 @@ class LlmProvider(str, Enum):
     BEDROCK = "Bedrock"
 
 
+llm_provider_types: list[LlmProvider] = list(LlmProvider)
+llm_provider_names: list[str] = [p.value.lower() for p in llm_provider_types]
+
+provider_base_urls: dict[LlmProvider, Optional[str]] = {
+    LlmProvider.OLLAMA: "http://localhost:11434",
+    LlmProvider.OPENAI: None,
+    LlmProvider.GROQ: None,
+    LlmProvider.ANTHROPIC: None,
+    LlmProvider.GOOGLE: None,
+    LlmProvider.BEDROCK: None,
+}
+
 provider_default_models: dict[LlmProvider, str] = {
-    LlmProvider.OLLAMA: "llama3.1:8b",
-    LlmProvider.OPENAI: "gpt-4o-2024-08-06",
+    LlmProvider.OLLAMA: "",
+    LlmProvider.OPENAI: "gpt-4o",
     LlmProvider.GROQ: "llama3-70b-8192",
     LlmProvider.ANTHROPIC: "claude-3-5-sonnet-20240620",
-    LlmProvider.GOOGLE: "gemini-pro",
+    LlmProvider.GOOGLE: "gemini-1.5-pro-002",
     LlmProvider.BEDROCK: "anthropic.claude-3-5-sonnet-20240620-v1:0",
 }
 
@@ -34,22 +59,69 @@ provider_env_key_names: dict[LlmProvider, str] = {
     LlmProvider.BEDROCK: "BEDROCK_API_KEY",
 }
 
-llm_provider_types: list[LlmProvider] = list(LlmProvider)
-provider_select_options: list[tuple[str, LlmProvider]] = [
-    (
-        p,
-        LlmProvider(p),
-    )
-    for p in llm_provider_types
-]
+
+def get_provider_name_fuzzy(provider: str) -> str:
+    """Get provider name fuzzy."""
+    provider = provider.lower()
+    for p in llm_provider_types:
+        if p.value.lower() == provider:
+            return p
+        if p.value.lower().startswith(provider):
+            return p
+    return ""
 
 
-def get_llm_provider_from_str(llm_provider_str: str) -> LlmProvider:
-    """Get the LLMProvider given a string matching one of its values."""
-    for llm_provider in LlmProvider:
-        if llm_provider.value == llm_provider_str:
-            return llm_provider
-    raise ValueError(f"Invalid LLM provider: {llm_provider_str}")
+@dataclass
+class LlmProviderConfig:
+    """Llm provider config."""
+
+    default_model: str
+    supports_base_url: bool
+    env_key_name: str
+
+
+provider_config: dict[LlmProvider, LlmProviderConfig] = {
+    LlmProvider.OLLAMA: LlmProviderConfig(
+        default_model="",
+        supports_base_url=True,
+        env_key_name=provider_env_key_names[LlmProvider.OLLAMA],
+    ),
+    LlmProvider.OPENAI: LlmProviderConfig(
+        default_model=provider_default_models[LlmProvider.OPENAI],
+        supports_base_url=True,
+        env_key_name=provider_env_key_names[LlmProvider.OPENAI],
+    ),
+    LlmProvider.GROQ: LlmProviderConfig(
+        default_model=provider_default_models[LlmProvider.GROQ],
+        supports_base_url=True,
+        env_key_name=provider_env_key_names[LlmProvider.GROQ],
+    ),
+    LlmProvider.ANTHROPIC: LlmProviderConfig(
+        default_model=provider_default_models[LlmProvider.ANTHROPIC],
+        supports_base_url=False,
+        env_key_name=provider_env_key_names[LlmProvider.ANTHROPIC],
+    ),
+    LlmProvider.GOOGLE: LlmProviderConfig(
+        default_model=provider_default_models[LlmProvider.GOOGLE],
+        supports_base_url=False,
+        env_key_name=provider_env_key_names[LlmProvider.GOOGLE],
+    ),
+    LlmProvider.BEDROCK: LlmProviderConfig(
+        default_model=provider_default_models[LlmProvider.BEDROCK],
+        supports_base_url=False,
+        env_key_name=provider_env_key_names[LlmProvider.BEDROCK],
+    ),
+}
+
+
+provider_env_key_names: dict[LlmProvider, str] = {
+    k: v.env_key_name for k, v in provider_config.items()
+}
+
+
+def provider_name_to_enum(name: str) -> LlmProvider:
+    """Get LlmProvider enum from string."""
+    return LlmProvider(name)
 
 
 def is_provider_api_key_set(provider: LlmProvider) -> bool:
@@ -62,3 +134,14 @@ def is_provider_api_key_set(provider: LlmProvider) -> bool:
 def get_providers_with_api_keys() -> list[LlmProvider]:
     """Get providers with API keys."""
     return [p for p in LlmProvider if is_provider_api_key_set(p)]
+
+
+def get_provider_select_options() -> list[tuple[str, LlmProvider]]:
+    """Get provider select options."""
+    return [
+        (
+            p,
+            LlmProvider(p),
+        )
+        for p in get_providers_with_api_keys()
+    ]
